28ecb9827b83ceec7fe143d85922ce353585dfac
Andrea Tongsak <atongsak@gmail.com>
2020-02-08 17:03:49 -0500

    Initial Commit

---
+++ SammyHACK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SammyHACK
+//
+//  Created by Andrea Tongsak on 2/8/20.
+//  Copyright © 2020 Andrea Tongsak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
