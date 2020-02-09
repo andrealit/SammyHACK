2e336523d1aebff77198878271f097e1dcd0f9c1
Andrea Tongsak <atongsak@gmail.com>
2020-02-09 04:49:35 -0500

    Added music

--- SammyHACK/ViewController.swift
+++ SammyHACK/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "sample", ofType: "mp3")!))
+            audioPlayer.prepareToPlay()
+        }
+        catch {
+            print(error)
+        }
+        
     }
-
-
+    
+    @IBAction func PlayB(_ sender: Any) {
+        audioPlayer.play()
+    }
+    
+    @IBAction func PauseB(_ sender: Any) {
+        if audioPlayer.isPlaying {
+            audioPlayer.pause()
+        } else {
+            
+        }
+    }
+    
+    @IBAction func RewindB(_ sender: Any) {
+        if audioPlayer.isPlaying {
+            audioPlayer.currentTime = 0;
+            audioPlayer.play()
+        } else {
+            audioPlayer.play()
+        }
+    }
+    
 }
 
